2a3ae86bd9b608343b6b950cb81fa09179789e8c
aeboyaci <ahmeterenboyaci45@gmail.com>
2021-11-05 00:16:32 +0300

    _error.js

--- pages/_error.js
+++ pages/_error.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from "../styles/Error.module.css";
 
 const Error = () => {
     return (
         <React.Fragment>
-            <div className={"text-center"}>
+            <div className={styles.height + " flex-column justify-content-center align-items-center"}>
                 <h1 className={"display-3"}>404</h1>
                 <h1 className={"display-4"}>Not Found!</h1>
             </div>
